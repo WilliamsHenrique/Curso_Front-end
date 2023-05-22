645ec9990e297f59d2eb2611550d77960e9ff02c
Williams Henrique <87279572+WilliamsHenrique@users.noreply.github.com>
2023-05-22 17:11:31 -0300

    exercicio_ts

---
+++ ex.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplica(numeroUm, numeroDois) {
+    return numeroUm * numeroDois;
+}
+const resultado = multiplica(5, 25);
+console.log(resultado);
+function dizNome(nome) {
+    return `Olá, ${nome}`;
+}
+const apresentacao = dizNome("Williams");
+console.log(apresentacao);
